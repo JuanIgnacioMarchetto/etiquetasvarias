30970a2cfb5c3a8ae44fd338ffdb8254630fbf8b
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-22 08:58:45 -0300

    22/3/2023-8:58

--- ejercicios clase 1.js
+++ ejercicios clase 1.js
@@ -1,63 +1,209 @@
-/* estos son los items a realizar :
-1. Declarar una variable de nombre “auto”, asignarle el valor “Corolla” y mostrarla por
-consola*.
+console.log('estoy en el archivo externo');
+console.log('3. Estoy en el archivo externo');
 
-2. Asignarle el valor “308” a la variable declarada en el ejercicio anterior y mostrarla por
-consola.
+/*
 
-3. A la misma variable de los ejercicios anteriores, asignarle el valor “C4”. Volver a
-mostrarla por consola.
+todo esto no lo ejecuta el navegador
+esto es un comentario de varias líneas
 
-4. Declarar dos variables “nombre” y “apellido”, asignarles tu nombre y apellido. Luego
-mostrar por consola ambos valores. Usar un solo console.log( )
+*/
 
-5. Dada una variable llamada “fruta” con el valor “naranja”. Declarar una variable
-“tipoDeLaVariableFruta” y asignarle como valor el tipo de la variable “fruta”. **
+//alert('Hola mundo');
 
-6. Para los ejercicios 1, 2 y 3, mostrar por consola el tipo de la variable en cada nueva
-asignación */
+console.log('==========================================================');
+console.log('Variables');
+console.log('==========================================================');
+
+let nombre; //declaración de una variable
+
+nombre = "Pepe"; //asignación de una variable con el símbolo =
+console.log(nombre);
+
+let apellido = 'Pérez'
+console.log(apellido)
 
 
-// Declaro la variable auto y le asigno el valor "Corolla"
-var auto = "Corolla";
+nombre = 'Juan';
+apellido = 'Paco'
 
-// Muestro por consola el valor de la variable auto
-console.log(auto);
+var provincia = 'Córdoba'
 
-// Muestro por consola el tipo de la variable auto
-console.log(typeof auto);
+console.log("Mi nombre es: " + nombre + ' y mi apellildo es: ' + apellido + " y vivo en la provincia de: " + provincia);
 
-// Le asigno a la variable auto el valor "308"
-auto = "308";
+console.log(`Mi nombre es ${nombre}`);
 
-// Muestro por consola el valor de la variable auto
-console.log(auto);
 
-// Muestro por consola el tipo de la variable auto
-console.log(typeof auto);
+//Constante
+const DNI = '123456789';// variables numéricas
+const IVA = 0.21;
 
-// Le asigno a la variable auto el valor
-auto = "C4";
+let falso = false;
+let verdadero = true;
 
-// Muestro por consola el valor de la variable auto
-console.log(auto);
+console.log(falso);
+console.log(verdadero);
 
-// Muestro por consola el tipo de la variable auto
-console.log(typeof auto);
 
-// Declaro la variable nombre y le asigno como valor mi nombre
-let nombre = "Juan Ignacio";
+//DNI = 876845454543;
 
-// Declaro la variable apellido y le asigno como valor mi apellido
-let apellido = "Marchetto";
+console.log('Mi dni es: ' + DNI);
 
-// Muestro por consola el valores de ambas variables
-console.log(nombre, apellido);
+let resultado = 1000 + IVA
 
-// Declaro la variable fruta y le asigno como valor "naranja"
-var fruta = "naranja";
+console.log(resultado);
 
-// Declaro la variable tipoDeLaVariableFruta y le asigno como valor el
-// resultado de evaluar la explesion typeof fruta, que nos retorna el tipo de
-// la variable fruta
-var tipoDeLaVariableFruta = typeof fruta;
+
+let pera = 'pera';
+let manzana = 'manzana';
+let limon = 'limon';
+
+//Arreglo - array - vector
+let frutas = ['pera', 'manzana', 'limon']; 
+console.log(frutas);
+
+let notas = [10 , 5, 8, 9, 10];
+console.log(notas);
+
+//índeice         0        1           2              3      4
+let persona = ['Pepe', 123456789, 'San Martín 2020', 15, 'Salta'];
+
+
+console.log(persona);
+
+console.log(persona.length)//indice n - 1 = 4
+console.log(persona[0]);
+console.log(persona[1]);
+console.log(persona[2]);
+console.log(persona[3]);
+console.log(persona[4]);
+//console.log(persona[5]);
+
+
+//Objetos JSON
+let tarjeta = {
+    tipo: 'Débito',
+    banco: 'Nación',
+    numero: 123456789,
+}
+
+console.log(tarjeta);
+console.log(tarjeta.banco);
+console.log(tarjeta.numero);
+console.log(tarjeta.tipo);
+
+
+console.log('==========================================================');
+console.log('Operadores Matemáticos');
+console.log('==========================================================');
+
+let uno = 25000;
+let dos = 80000;
+
+resultado = uno + dos;
+console.log(`El resultado de la SUMA es: ${resultado}`);
+
+resultado = dos - uno;
+console.log(`El resultado de la RESTA es: ${resultado}`);
+
+resultado = uno * dos;
+console.log(`El resultado de la MULTIPLICACIÓN es: ${resultado}`);
+
+resultado = dos / uno;
+console.log(`El resultado de la DIVISIÓN es: ${resultado}`);
+
+
+resultado = dos % uno;//módulo: es el resto de la división
+console.log(`El resultado de la MÓDULO es: ${resultado}`);
+
+
+console.log('==========================================================');
+console.log('Condicioneles');
+console.log('==========================================================');
+
+if (true) {
+    console.log('Verdadero, escribo');
+}
+
+if (false) {
+    console.log('Nunva voy a escribir');
+}
+
+
+let edad = 19;
+
+if (edad > 18) {///operadores relacionales
+    console.log('Podés viajar');
+}
+
+if (edad < 18) {
+    console.log('Podés viajar acompañado de un adulto');
+}
+
+if (edad == 18) {
+    console.log('Falta poco para que viajes');
+}
+
+
+console.log('==========================================================');
+
+let saldo = 0;
+
+if (saldo > 0) {
+    console.log('Podés comprar');
+}else{
+    console.log('No podés comprar');
+}
+
+if (saldo >= 5000) {
+    console.log('Podés comprar');
+}
+
+if (saldo > 5001) {
+    console.log('Podés comprar');
+}else{
+    console.log('No podés comprar');
+}
+
+console.log('==========================================================');
+console.log('Operadores lógicos');
+console.log('==========================================================');
+
+//y lógico: todas las evaluaciones tienen que ser verdaderas
+if (edad > 18 && saldo > 0) {
+    console.log('Podés viajar y comprar');
+}else{
+    console.log('No podés viajar ni comprar');
+}
+
+//el o lógico: necesita que una de las condiciones sea verdadera
+if (edad > 18 || saldo > 0) {
+    console.log('Podés viajar y comprar');
+}else{
+    console.log('No podés viajar ni comprar');
+}
+
+
+console.log('==========================================================');
+
+let user = 'pepe';
+let pass = 'jkajkasjkgf';
+let reco = false;
+
+if (user == 'pepe' && pass == '12345' && reco == true) {
+    console.log('Podés ingresar a tu cuenta bancaria');
+}else{
+    console.log('No Podés ingresar a tu cuenta bancaria');
+}
+
+
+if (user == 'pepe' || pass == '12345' || reco == true) {
+    console.log('Podés ingresar a tu cuenta bancaria');
+}else{
+    console.log('No podés viajar ni comprar');
+}
+
+
+
+console.log('==========================================================');
+console.log('Funciones');
+console.log('==========================================================');
